1480dc93cc38aac07060575760c8acafad7f6525
viktorahlstroem <sno.kattunge@gmail.com>
2015-07-19 00:51:43 -0400

    Added files.

---
+++ vars.java
@@ -0,0 +1,20 @@
+public class vars {
+  public static void main(String[] args) {
+    int a = 99;
+    float pi = (float) 3.14;
+    char b = 'c';
+    String partone = "Well ";
+    String parttwo = "then.";
+    String combined = partone + parttwo;
+    boolean boo = true;
+    System.out.println(a);
+    System.out.println(pi);
+    System.out.println(b);
+    System.out.println(partone);
+    System.out.println(parttwo);
+    System.out.println(combined);
+    System.out.println(boo);
+  }
+}
+
+
