1221c89ca474c5a2acd32c46841e1b2c4427f565
Viktor Ahlström <sno.kattunge@gmail.com>
2015-07-22 00:09:29 -0400

    + floor.java

---
+++ floor.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.util.Locale;
+import java.text.NumberFormat;
+import java.text.DecimalFormat;
+public class floor {
+    public static void main(String[] args) {
+        String dimensions;
+        Scanner a = new Scanner( System.in );
+        Scanner b = new Scanner( System.in );
+        Scanner c = new Scanner( System.in );
+        System.out.println("Enter the dimensions of the floor. [w*h]");
+        dimensions = a.next();
+        String[] wh = dimensions.split("\\*");
+        
+        float w = Float.parseFloat(wh[0]);
+        float h = Float.parseFloat(wh[1]);
+        float f = (w*h);
+
+        System.out.println("Assuming square tile, enter its side length.");
+        String xb = b.next();
+        System.out.println("Enter the cost of one tile.");
+        String yb = c.next();
+        
+        float x = Float.parseFloat(xb);
+        float y = Float.parseFloat(yb);
+        
+        float e = (x*x);
+        float z = (f/e);
+        
+        if (f%e != 0) {
+            System.out.println("NOT A WHOLE");
+            double j = Math.ceil(z) - 1;
+            double d = z - j;
+            double l = 1 - d; 
+            z = Math.round(z);
+            System.out.println(d);
+            DecimalFormat df = new DecimalFormat("#######");
+            String zz = df.format(z);
+            System.out.println(zz);
+            
+            boolean isWhole = false;
+        } else {
+            z = Math.round(z);
+        }
+        float m = (z*y);
+        double v = m;
+        Locale lc = new Locale("en", "US");
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(lc);
+        System.out.println("You would have to buy " + z + " tiles, which would total " + currencyFormatter.format(v) + ".");
+
+    
+    }
+}
